94d547a73f29358cbe20caa6aeaa1ff8f611dc43
Matheus Amaral <m.amaral8998@gmail.com>
2022-09-12 14:56:25 -0300

    projeto flask

---
+++ comunidadeteteu/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '012370f38252f3114ea5bd265b19b603'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Faça o login para continuar!'
+login_manager.login_message_category = 'alert-info'
+
+from comunidadeteteu import routes
